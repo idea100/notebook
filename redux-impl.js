b065ebd0aff273e9e435d983ab9f8b7c534bf546
idea100 <517626980@qq.com>
2018-03-08 00:44:06 +0800

    create redux-impl.js

---
+++ redux-impl.js
@@ -0,0 +1,51 @@
+const createStore = reducer => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({})
+
+  return { getState, dispatch, subscribe };
+}
+
+
+function counter(state = 0, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1;
+  case 'DECREMENT':
+    return state - 1;
+  default:
+    return state;
+  }
+}
+
+// 创建 Redux store 来存放应用的状态。
+// API 是 { subscribe, dispatch, getState }。
+let store = createStore(counter);
+
+// 可以手动订阅更新，也可以事件绑定到视图层。
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+// 改变内部 state 惟一方法是 dispatch 一个 action。
+// action 可以被序列化，用日记记录和储存下来，后期还可以以回放的方式执行
+store.dispatch({ type: 'INCREMENT' });
+// 1
+store.dispatch({ type: 'INCREMENT' });
+// 2
+store.dispatch({ type: 'DECREMENT' });
